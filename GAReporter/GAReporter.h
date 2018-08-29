f40fe38a101c689f0bded68740270bca0dddb005
吳登秝 <ji3g4kami@gmail.com>
2018-08-29 16:24:04 +0800

    Initial Commit

---
+++ GAReporter/GAReporter.h
@@ -0,0 +1,19 @@
+//
+//  GAReporter.h
+//  GAReporter
+//
+//  Created by udn on 2018/8/29.
+//  Copyright © 2018年 dengli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GAReporter.
+FOUNDATION_EXPORT double GAReporterVersionNumber;
+
+//! Project version string for GAReporter.
+FOUNDATION_EXPORT const unsigned char GAReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GAReporter/PublicHeader.h>
+
+
