7333587ec1b75190669c47b74b240d47d7e377c3
Ronaldo <ronaldosf.sf@gmail.com>
2019-01-16 15:23:05 -0300

    Teste de header

---
+++ GAReporter/GAReporter-umbrella.h
@@ -0,0 +1,23 @@
+//
+//  GAReporter-umbrella.h
+//  GAReporter
+//
+//  Created by Ronaldo S F on 16/01/19.
+//  Copyright © 2019 dengli. All rights reserved.
+//
+
+#ifndef GAReporter_umbrella_h
+#define GAReporter_umbrella_h
+
+#endif /* GAReporter_umbrella_h */
+
+#import "GAI.h"
+#import "GAIDictionaryBuilder.h"
+#import "GAIEcommerceFields.h"
+#import "GAIEcommerceProduct.h"
+#import "GAIEcommerceProductAction.h"
+#import "GAIEcommercePromotion.h"
+#import "GAIFields.h"
+#import "GAILogger.h"
+#import "GAITrackedViewController.h"
+#import "GAITracker.h"
