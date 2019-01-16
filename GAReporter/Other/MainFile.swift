54008ce141d4d54087eb9f833e38bd4c1ce47725
Ronaldo <ronaldosf.sf@gmail.com>
2019-01-16 13:33:20 -0300

    Adicionado arquivo para teste

---
+++ GAReporter/Other/MainFile.swift
@@ -0,0 +1,21 @@
+//
+//  MainFile.swift
+//  NACardSDK
+//
+//  Created by Ronaldo S F on 16/01/19.
+//  Copyright © 2019 Natura. All rights reserved.
+//
+
+import Foundation
+
+
+public class MainFile {
+    
+    public func teste() {
+        
+    }
+    
+    public func teste2() {
+        
+    }
+}
